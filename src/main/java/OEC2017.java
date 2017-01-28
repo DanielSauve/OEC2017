86e74056f31d6101b0ac2f9d870d3e91dad0675d
chrisB25 <cbriglio@live.ca>
2017-01-28 01:02:44 -0500

    made modifications for command line interface

--- src/main/java/OEC2017.java
+++ src/main/java/OEC2017.java
@@ -5,6 +5,7 @@
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class OEC2017 {
     public static void main(String[] args){
@@ -14,6 +15,24 @@ public class OEC2017 {
         HashMap<String, Generator> generators = reader.readGenerators();
         HashMap<String, Node> nodes = reader.readNode();
         ArrayList<Link> links = reader.readLink(houses, nodes, generators);
+
+        ArrayList<SuperNode> graph = new ArrayList<SuperNode>();
+        graph.addAll(houses.values());
+        graph.addAll(generators.values());
+        graph.addAll(nodes.values());
+
+        PathPlanner planner = new PathPlanner((List<Generator>) generators.values(), graph);
+
+        for (int i = 1; i <= 8;i++) {
+            for (House house:houses.values()) {
+                if (house.getOn(i) == 1) {
+                    List<SuperNode> path = planner.findPath(house);
+                    Float cost = planner.findCost(house);
+                }
+
+            }
+        }
+
 
     }
 }
